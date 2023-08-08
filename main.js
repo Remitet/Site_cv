abab03cd5580efc0cfac511983760139b49b7a98
Тимур Калинин <t.kalinin@polymatica.com>
2023-08-08 18:21:23 +0300

    fix

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
-jQuery(document).ready(function($) {
-
-   
-        AOS.init({
-            offset: 100,
-            once: true,
-            duration: 800,
-            throttleDelay: 50,
-            disable: window.innerWidth < 768
-        });
-   
-
+jQuery(document).ready(function ($) {
+  AOS.init({
+    offset: 100,
+    once: true,
+    duration: 800,
+    throttleDelay: 50,
+    disable: window.innerWidth < 768,
+  });
 });
